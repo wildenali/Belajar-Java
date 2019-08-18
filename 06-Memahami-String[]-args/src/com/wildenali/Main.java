96f39123a587dd65a5a8190c1a9c7f86d117e708
wildenali <wildenali@github.com>
2019-08-18 16:20:39 +0700

    cara menggunakan String[] args seperti apa

---
+++ 06-Memahami-String[]-args/src/com/wildenali/Main.java
@@ -0,0 +1,12 @@
+package com.wildenali;
+
+public class Main {
+
+    public static void main(String[] args) {        // String[] adalah input yang bisa kita masukan di dalam program java
+                                                    // String[] adalah sebuah data yang memiliki indeks array
+        System.out.println(args[0]);
+//        System.out.println(args[1]);
+        System.out.println("Helow kawan-kawan " + args[0]);
+    }
+
+}
