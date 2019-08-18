5e3f5ffe4b0a791d77b0fefe0d108cf97976e97c
wildenali <wildenali@github.com>
2019-08-18 14:59:11 +0700

    membandingkan dengan project 03 yang manual dengan menggunakan Intellij IDEA

--- 04-Program-dengan-IDE/src/com/wilden/Main.java
+++ 04-Program-dengan-IDE/src/com/wilden/Main.java
@@ -3,7 +3,7 @@ package com.wilden;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Helow lo000ww");
+        System.out.println("Helow loOOWW");
     }
 
 }
