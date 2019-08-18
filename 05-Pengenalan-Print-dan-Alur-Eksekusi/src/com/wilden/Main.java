c4375001c518970c902cef1f2669ebba69313991
wildenali <wildenali@github.com>
2019-08-18 16:00:56 +0700

    Pengenalan Print dan Alur Eksekusi program

---
+++ 05-Pengenalan-Print-dan-Alur-Eksekusi/src/com/wilden/Main.java
@@ -0,0 +1,13 @@
+package com.wilden;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow 05 Pengenalan Alur Eksekusi");
+        System.out.println("Hellow ini baris ke dua");
+        System.out.print("ini Baris ketiga");
+        System.out.print("ini Baris keempat \n");
+        System.out.printf("wiro sableng %d", 212);
+    }
+
+}
