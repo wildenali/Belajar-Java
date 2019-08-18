d1e7591a257a184f5a399d274685c2f89708b788
wildenali <wildenali@github.com>
2019-08-19 01:47:41 +0700

    belajar variable, assignment, dan deklarasi

---
+++ 07-Variables-Assignment-dan-Deklarasi/src/com/wildenali/Main.java
@@ -0,0 +1,28 @@
+package com.wildenali;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ini adalah komentar
+        /*
+        Komen multiline
+        iya kan
+         */
+
+        System.out.println("Hello dan run");
+
+        // Variable
+        int x = 10;     // assignment
+        System.out.println("ini adalah variable " + x);
+
+        x = 20;
+        System.out.println("ini adalah variable " + x);
+
+        // Deklarasi
+        int b;          // Declaration
+        b = 89;
+        System.out.println("ini adalah deklarasi " + b);
+
+    }
+
+}
