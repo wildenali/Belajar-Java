548a24d63cbfe90bc35225f5cfbd48e4d42d6e43
wildenali <wildenali@github.com>
2019-08-18 12:29:45 +0700

    memahami isi dalam program si java, kaya mengenal class dan main

---
+++ 02-Memahami-Program/OtongSurotong.java
@@ -0,0 +1,12 @@
+// public adalah keyword visibility
+// class adalah ya kelas
+// OtongSurotong adalah identifier, nama ini harus sama dengan nama filenya, misalnya ini nama file nya adalah OtongSurotong.java JADI identifier nya adalah OtongSurotong
+public class OtongSurotong{
+
+  // public adalah visibiliti, ada public ada private (tidak dibuka untuk umum)
+  // main adalah default java, pokonya java harus ada main
+  public static void main(String[] args) {
+    System.out.println("Otong Otongan");    // ini adalah isi program nya si OtongSurotong, cuma nampilin di console aja
+  }
+
+}
