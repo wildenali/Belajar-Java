50676e029340bdb0da4aea71c9d5ba0c6a96987d
wildenali <wildenali@github.com>
2019-08-18 13:43:02 +0700

    kenapa di java ada src, bin, dan lain lainnya

---
+++ 03-Organisasi-Program-dengan-Package/src/com/wilden/Main.java
@@ -0,0 +1,9 @@
+package com.wilden;
+
+public class Main{
+
+  public static void main(String[] args) {
+    System.out.println("Hai World Otong");
+  }
+
+}
---
+++ 03-Organisasi-Program-dengan-Package/src/programku/OtongSurotong.java
@@ -0,0 +1,9 @@
+package programku;
+
+public class OtongSurotong{
+
+  public static void main(String[] args) {
+    System.out.println("Otong Otongan");    // ini adalah isi program nya si OtongSurotong, cuma nampilin di console aja
+  }
+
+}
