1e08fd929780a961df11419b80417559d170ac11
wildenali <wildenali@github.com>
2019-08-19 11:47:16 +0700

    If else if statement

---
+++ 19-IF-ELSE-IF-Statement/src/com/wildenali/Main.java
@@ -0,0 +1,21 @@
+package com.wildenali;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        System.out.println("Ini adalah awal program");
+
+        if (a == 5) {
+            System.out.println("ini adalah aksi 1");
+        } else if (a == 10) {
+            System.out.println("ini adalah aksi 2");
+        } else {
+            System.out.println("ini adalah aksi default");
+        }
+
+        System.out.println("ini adalah akhir program");
+
+    }
+}
