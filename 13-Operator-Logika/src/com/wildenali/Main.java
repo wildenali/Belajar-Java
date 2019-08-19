4750d551b654d7f0ea58ebb445a4da37b80ed528
wildenali <wildenali@github.com>
2019-08-19 11:39:18 +0700

    Operator Logika AND &&, OR ||, XOR ^, NOT ~

---
+++ 13-Operator-Logika/src/com/wildenali/Main.java
@@ -0,0 +1,72 @@
+package com.wildenali;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean a, b, c;
+
+        System.out.println("===== OR || =====");
+        a = false;
+        b = false;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a || b);
+        System.out.println(a + " || " + b + " = " + c);
+
+        System.out.println("\n===== AND && =====");
+        a = false;
+        b = false;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a && b);
+        System.out.println(a + " && " + b + " = " + c);
+
+        System.out.println("\n===== XOR ^ , Exlusive OR =====");
+        a = false;
+        b = false;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = false;
+        b = true;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = true;
+        b = false;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+        a = true;
+        b = true;
+        c = (a ^ b);
+        System.out.println(a + " ^ " + b + " = " + c);
+
+        System.out.println("\n===== NOT / Negasi ! , flipping =====");
+        a = true;
+        c = !a;
+        System.out.println(a + " --> (!) = " + c);
+        a = false;
+        c = !a;
+        System.out.println(a + " --> (!) = " + c);
+
+    }
+}
