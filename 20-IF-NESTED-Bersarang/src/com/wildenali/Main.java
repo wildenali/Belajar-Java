42ed394367c28188433f61461212f4685a964a52
wildenali <wildenali@github.com>
2019-08-19 11:51:56 +0700

    If berasarang, if dalam if

---
+++ 20-IF-NESTED-Bersarang/src/com/wildenali/Main.java
@@ -0,0 +1,23 @@
+package com.wildenali;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        System.out.println("ini adalah awal dari program");
+
+        if (a == 5) {
+            if (b == 10) {
+                System.out.println("ini adalah dimana a = 5 dan b = 10");
+            } else {
+                System.out.println("ini adalah dimana a = 5 dan b bukan 10");
+            }
+        } else {
+            System.out.println("ini adalah dimana a salah");
+        }
+
+        System.out.println("ini adalah akhir program");
+    }
+}
