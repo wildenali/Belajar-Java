71538143c1f93cc06fdd8b0af836f44b07f1bcb6
wildenali <wildenali@github.com>
2019-08-19 11:46:36 +0700

    Statement IF

---
+++ 18-IF-Statement/src/com/wildenali/Main.java
@@ -0,0 +1,19 @@
+package com.wildenali;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println("Nilai = " + a);
+
+        if (a == 19) {
+            System.out.println("Ini adalah jalur TRUE");
+        } else {
+            System.out.println("Ini adalah jalur FALSE");
+        }
+
+        System.out.println("Selesaii......");
+
+    }
+}
