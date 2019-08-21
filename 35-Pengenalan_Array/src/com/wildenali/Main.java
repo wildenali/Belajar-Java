7b144ea90be0e9ec5d7d266d442935521d74eae0
wildenali <wildenali@github.com>
2019-08-21 15:02:08 +0700

    Pengenalan Array dan sebagian fungsi dan latihannya

--- 35-Pengenalan_Array/src/com/wildenali/Main.java
+++ 35-Pengenalan_Array/src/com/wildenali/Main.java
@@ -66,6 +66,37 @@ public class Main {
         }
 
 
+        System.out.println("\n\n\nArray Lanjutan");
 
+        int[] arrayAngka1 = {1,2,3,4,5};
+        int[] arrayAngka2 = new int[5];
+
+        System.out.println(arrayAngka1);
+        System.out.println(arrayAngka2);
+        System.out.println();
+
+        arrayAngka2 = arrayAngka1;
+
+        System.out.println(arrayAngka1);
+        System.out.println(arrayAngka2);
+
+        System.out.println("array 1 -> " + Arrays.toString(arrayAngka1));
+        System.out.println("array 2 -> " + Arrays.toString(arrayAngka2));
+
+        arrayAngka1[0] = 100;
+        arrayAngka1[4] = 400;
+        System.out.println("array 1 -> " + Arrays.toString(arrayAngka1));
+        System.out.println("array 2 -> " + Arrays.toString(arrayAngka2));
+
+        ubahArray(arrayAngka1);
+        System.out.println("array 1 -> " + Arrays.toString(arrayAngka1));
+        System.out.println("array 2 -> " + Arrays.toString(arrayAngka2));
+
+    }
+
+    // method yang argument nya adalah reference, pass by reference
+    // bukan pass by value
+    private static void ubahArray(int[] dataArray) {
+        dataArray[0] = 125;
     }
 }
