672f437c434b00b557c17e2a9ff82a1d78eb7bfc
wildenali <wildenali@github.com>
2019-08-25 14:37:40 +0700

    Perbedaan antara Byte Stream dan Character Stream

---
+++ 48-Character-Stream-Input-Output/src/com/wildenali/Main.java
@@ -0,0 +1,51 @@
+package com.wildenali;
+
+import com.sun.javaws.IconUtil;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        // ============= Buka File =============
+        // byte stream -> FileInputStream
+        // Character stream -> FileReader
+        FileInputStream byteFileInput = new FileInputStream("input.txt");
+        FileReader      charFileInput = new FileReader("input.txt");
+
+        FileOutputStream byteFileOutput = new FileOutputStream("outputByte.txt");
+        FileWriter       charFileOutput = new FileWriter("outputChar.txt");
+
+        // ============= Baca File =============
+//        System.out.println((char)byteFileInput.read());
+//        System.out.println((char)charFileInput.read());
+//        System.out.println((char)byteFileInput.read());
+//        System.out.println((char)charFileInput.read());
+
+        // byte File
+        int buffer = byteFileInput.read();
+        while (buffer != -1) {
+            System.out.print((char)buffer);
+            byteFileOutput.write(buffer);
+            buffer = byteFileInput.read();
+        }
+
+        System.out.println("\n\n");
+
+        // char File
+        buffer = charFileInput.read();
+        while (buffer != -1) {
+            System.out.print((char)buffer);
+            charFileOutput.write(buffer);
+            buffer = charFileInput.read();
+        }
+
+        // ============= Tutup File =============
+        byteFileInput.close();
+        charFileInput.close();
+        byteFileOutput.close();
+        charFileOutput.close();
+
+    }
+}
